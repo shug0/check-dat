b2ef4b4de3822ed74dcd2b1d01d810416cab7c12
Thomas Alberola <thomas@needacoffee.fr>
2018-08-19 15:16:43 +0200

    fix(exports) correct way of exporting common components

--- src/app/components/common/index.js
+++ src/app/components/common/index.js
@@ -1,5 +1,13 @@
-export Button from './Button'
-export Card from './Card'
-export Input from './Input'
-export Avatar from './Avatar'
-export Icon from './Icon'
+import Button from './Button'
+import Card from './Card'
+import Input from './Input'
+import Avatar from './Avatar'
+import Icon from './Icon'
+
+export {
+  Button,
+  Card,
+  Input,
+  Avatar,
+  Icon
+}
